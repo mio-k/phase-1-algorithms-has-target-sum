1f3ed0ac48f9599c9b39669d07f2447d00add511
mio kataoka <mio.kanna@gmail.com>
2022-03-26 13:30:57 -0700

    commit tree ab8db73f50a93057ee9d4fbc38ccad31143b7175

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
+
+/* 
+take one number from the array
+add it with the first number in the array 
+compare the resulting value against the target value
+if they are the same, return true
+===
+loop through the numbers in the array
+if the number you get by subtracting the i number from the target number is one of the number in the array
+return true
+
+*/
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let result = [];
+  if (array.length === 1){
+    return (false) 
+  } else {
+  for (let i=0; i < array.length; i++){
+    for (let j = i+1; j < array.length; j++){
+      if (target == array[i] + array[j]) {
+        result.push("true")
+      } 
+    }
+  }
+     if (result.includes("true")){
+       return true
+     } else{
+       return false
+     }
 }
+}
+
 
 /* 
   Write the Big O time complexity of your function here
+
 */
 
 /* 
   Add your pseudocode here
+Starting from the first item of the array and loop through the whole array(i),
+also start another loop from the next number in the array(j),
+add the two number with each iteration, and compare it against the target value.
 */
 
 /*
   Add written explanation of your solution here
+  take one number from the array
+add it with the first number in the array 
+compare the resulting value against the target value
+if they are the same, return true.
+then move on to the second number in the array to do the same.
+repeat until you get to the last number of the array.
 */
 
 // You can run `node index.js` to view these console logs
